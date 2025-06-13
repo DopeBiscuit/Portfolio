dbcae6b89af42637737384d6606c870e1d41b0cd
DopeBiscuit <abdulrahman.hany003@gmail.com>
2025-06-13 21:02:13 +0300

    removed resume

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
             <Projects />
             <Freelance />
             <Achievements />
-            <Resume />
+            {/* <Resume /> */}
             <Contact />
           </main>
           <ScrollToTop />
